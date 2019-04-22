96f05fdefaefbcca3cc0894f3c7991449470e058
nxnjz <nxnjz@nxnjz.net>
2019-04-22 06:01:39 +0300

    fixed copyright notice

--- src/rblib.rs
+++ src/rblib.rs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2019 nxnjz.
+/* Copyright (C) 2019 A. Karl. W.
 This file is part of RustBuster.
 RustBuster is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
